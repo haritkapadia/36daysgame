e601d6e87cd65ada0e0050f295cbfe34459157d6
Harit Kapadia <haritkapadia@rocketmail.com>
2019-05-21 12:31:14 -0400

    Added comments and the JavaFX library.

--- src/SurvivalGuidePane.java
+++ src/SurvivalGuidePane.java
@@ -1,3 +1,9 @@
+/**
+ * Harit Kapadia, Jack Farley
+ * Ms. Krasteva
+ * 2019/May/21
+ */
+
 import java.util.*;
 import javafx.application.*;
 import javafx.scene.*;
@@ -8,9 +14,20 @@ import javafx.scene.paint.*;
 import javafx.scene.shape.*;
 import javafx.stage.*;
 
+/**
+ * A pane displaying the survival guide.
+ *
+ * @author Harit Kapadia, Jack Farley
+ */
 public class SurvivalGuidePane extends HBox {
 	private Stage stage;
 
+	/**
+	 * The class constructor.
+	 * The pane is initialised with placeholders for the survival guide.
+	 *
+	 * @param stage The window on which the pane will be displayed.
+	 */
 	SurvivalGuidePane(Stage stage) {
 		this.stage = stage;
 		getChildren().add(new VBox());
@@ -21,26 +38,26 @@ public class SurvivalGuidePane extends HBox {
 	}
 }
 
-class SurvivalGuideBook {
+// class SurvivalGuideBook {
 
-}
+// }
 
-class SurvivalGuideSection {
-	private String title;
-	private SurvivalGuideContent[] content;
+// class SurvivalGuideSection {
+//	private String title;
+//	private SurvivalGuideContent[] content;
 
-	SurvivalGuideSection(String title, SurvivalGuideContent[] content) {
-		this.title = title;
-		this.content = content;
-	}
-}
+//	SurvivalGuideSection(String title, SurvivalGuideContent[] content) {
+//		this.title = title;
+//		this.content = content;
+//	}
+// }
 
-class SurvivalGuideContent {
-	private String text;
-	private String type;
+// class SurvivalGuideContent {
+//	private String text;
+//	private String type;
 
-	SurvivalGuideContent(String text, String type) {
-		this.text = text;
-		this.type = type;
-	}
-}
+//	SurvivalGuideContent(String text, String type) {
+//		this.text = text;
+//		this.type = type;
+//	}
+// }
