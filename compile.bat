62ade96cf3aedde80d6e933fab136102f2fbaa9c
Harit Kapadia <haritkapadia@rocketmail.com>
2019-05-20 21:13:19 -0400

    Started GUI creation and added JavaFX

--- compile.bat
+++ compile.bat
@@ -1,1 +1,2 @@
-javac -d bin src\Main.java
+set FX_LIB=javafx-sdk-11.0.2\lib
+javac --module-path %FX_LIB% --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web -d bin src\*.java
--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-java -cp ";bin;" Main
+set FX_LIB=javafx-sdk-11.0.2\lib
+java -cp ";bin;" --module-path %FX_LIB% --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web Main
