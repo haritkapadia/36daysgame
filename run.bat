71c660a355a8bcd66e087f7d1e978f040ef15cfe
Harit Kapadia <haritkapadia@rocketmail.com>
2019-06-07 13:56:30 -0400

    played the game

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 set FX_LIB=javafx-sdk-12.0.1\lib
-java -cp ";bin;" --module-path %FX_LIB% --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web Main
+java -cp ";bin;" --module-path %FX_LIB% --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web -Xmx4G Main
